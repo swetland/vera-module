c89b08ea1d493a2481dd4cb4110dffdc10c395ad
Brian Swetland <swetland@frotz.net>
2023-08-09 18:21:01 -0700

    fpga/build: import build system from gateware repository

---
+++ fpga/build/init.mk
@@ -0,0 +1,38 @@
+## Copyright 2018 Brian Swetland <swetland@frotz.net>
+##
+## Licensed under the Apache License, Version 2.0 
+## http://www.apache.org/licenses/LICENSE-2.0
+
+VERILATOR := verilator
+NEXTPNR_ICE40 := nextpnr-ice40
+NEXTPNR_ECP5 := nextpnr-ecp5
+YOSYS := yosys
+ICEPACK := icepack
+ECPPACK := ecppack
+
+VIVADOPATH := /work/xilinx/Vivado/2019.2
+XSDKPATH := /work/xilinx/SDK/2019.2
+
+VIVADO := $(VIVADOPATH)/bin/vivado
+XELAB := $(VIVADOPATH)/bin/xelab
+XSIM := $(VIVADOPATH)/bin/xsim
+XMD := $(XSDKPATH)/bin/xmd
+
+ALL_BUILDS :=
+ALL_TARGETS :=
+
+define project
+$(eval PROJECT_DEF := $1)\
+$(eval PROJECT_TYPE :=)\
+$(eval PROJECT_PART :=)\
+$(eval PROJECT_SRCS :=)\
+$(eval PROJECT_VOPTS :=)\
+$(eval PROJECT_VERILOG_DEFS :=)\
+$(eval PROJECT_NEXTPNR_OPTS :=)\
+$(eval include $(PROJECT_DEF))\
+$(eval PROJECT_NAME := $(patsubst project/%.def,%,$(PROJECT_DEF)))\
+$(eval pr-inc := $(wildcard $(patsubst %,build/%.mk,$(PROJECT_TYPE))))\
+$(if $(pr-inc),,$(error $1: unknown project type: "$(PROJECT_TYPE)"))\
+$(eval include $(pr-inc))
+endef
+
---
+++ fpga/build/verilator-sim.mk
@@ -0,0 +1,47 @@
+## Copyright 2018 Brian Swetland <swetland@frotz.net>
+##
+## Licensed under the Apache License, Version 2.0 
+## http://www.apache.org/licenses/LICENSE-2.0
+
+PROJECT_OBJDIR := out/-vsim-/$(PROJECT_NAME)
+PROJECT_RUN := $(PROJECT_NAME)-vsim
+PROJECT_BIN := out/$(PROJECT_NAME)-vsim
+
+PROJECT_VLG_SRCS := $(filter %.v %.sv,$(PROJECT_SRCS)) 
+
+PROJECT_OPTS := --top-module testbench
+PROJECT_OPTS += --Mdir $(PROJECT_OBJDIR)
+PROJECT_OPTS += --exe ../../src/testbench.cpp ../../src/sim-sdram.cpp
+PROJECT_OPTS += --cc
+PROJECT_OPTS += -o ../../$(PROJECT_NAME)-vsim
+PROJECT_OPTS += -DSIMULATION
+PROJECT_OPTS += $(PROJECT_VOPTS)
+
+PROJECT_OPTS += -CFLAGS -DTRACE --trace
+
+$(PROJECT_BIN): _NAME := $(PROJECT_NAME)
+$(PROJECT_BIN): _SRCS := $(PROJECT_VLG_SRCS)
+$(PROJECT_BIN): _OPTS := $(PROJECT_OPTS)
+$(PROJECT_BIN): _DIR := $(PROJECT_OBJDIR)
+
+$(PROJECT_BIN): $(PROJECT_SRCS) $(PROJECT_DEF) src/testbench.cpp src/sim-sdram.cpp
+	@mkdir -p $(_DIR) bin
+	@echo "COMPILE (verilator): $(_NAME)"
+	@$(VERILATOR) $(_OPTS) $(_SRCS)
+	@echo "COMPILE (C++): $(_NAME)"
+	make -C $(_DIR) -f Vtestbench.mk
+
+$(PROJECT_NAME): $(PROJECT_BIN)
+
+$(PROJECT_RUN): _LOGFILE := out/sim/$(PROJECT_NAME).log
+$(PROJECT_RUN): _VCDFILE := out/sim/$(PROJECT_NAME).vcd
+$(PROJECT_RUN): $(PROJECT_BIN)
+	@mkdir -p out/sim
+	@$< -trace $(_VCDFILE) > $(_LOGFILE)
+
+ALL_TARGETS += $(PROJECT_NAME) $(PROJECT_RUN) 
+ALL_BUILDS += $(PROJECT_NAME)
+
+TARGET_$(PROJECT_NAME)_DESC := build verilator sim: $(PROJECT_BIN)
+TARGET_$(PROJECT_RUN)_DESC := run verilator sim: $(PROJECT_BIN)
+
